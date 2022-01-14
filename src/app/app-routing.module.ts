37af9822cb9a2c51a72c3e85dd12915d2cc5217e
altkim <altkimiadigital@gmail.com>
2022-01-14 05:45:21 -0700

    Seccion 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, Component } from '@angular/core';
 
-const routes: Routes = [];
+import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
+
+ /* import{ Routes } from 'angular/router'; */
+
+const app_routes: Routes = [
+  { path: 'home', component: PortafolioComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'item', component: ItemComponent }, 
+  { path: 'contact', component: ContactComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(app_routes, { useHash: true } )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
